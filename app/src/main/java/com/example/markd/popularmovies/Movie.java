a6ed16481fe1a464ac8e94850ca35f4d1fd7a286
markdagraca <markdagraca1@gmail.com>
2018-12-08 19:50:51 -0800

    Revert "Merge branch 'master' of https://github.com/markdagraca/PopularMovies into johns_branch"
    This reverts commit 355826cb186047be630dba85e161da16312cf907, reversing
    changes made to 23d4befde15f0a93f06fb18b51396e4274e176eb.

--- app/src/main/java/com/example/markd/popularmovies/Movie.java
+++ app/src/main/java/com/example/markd/popularmovies/Movie.java
@@ -3,11 +3,7 @@ package com.example.markd.popularmovies;
 import java.util.Date;
 
 public class Movie {
-<<<<<<< HEAD
     public Movie(String id, String posterUrl, String title, String description, double popularity, String releaseDate) {
-=======
-    public Movie(String id, String posterUrl, String title, String description, float popularity, Date releaseDate) {
->>>>>>> dec714f06e996c9fa1732203cfb5bc59787a8f26
         this.id = id;
         this.posterUrl = posterUrl;
         this.title = title;
@@ -32,19 +28,11 @@ public class Movie {
         return description;
     }
 
-<<<<<<< HEAD
     public double getPopularity() {
         return popularity;
     }
 
     public String getReleaseDate() {
-=======
-    public float getPopularity() {
-        return popularity;
-    }
-
-    public Date getReleaseDate() {
->>>>>>> dec714f06e996c9fa1732203cfb5bc59787a8f26
         return releaseDate;
     }
 
@@ -52,13 +40,8 @@ public class Movie {
     private String posterUrl;
     private String title;
     private String description;
-<<<<<<< HEAD
     private double popularity;
     private String releaseDate;
-=======
-    private float popularity;
-    private Date releaseDate;
->>>>>>> dec714f06e996c9fa1732203cfb5bc59787a8f26
 
 
 
